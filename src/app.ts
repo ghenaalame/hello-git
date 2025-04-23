29eced4c84646eccd7016162d1cdbc784f4c4264
ghenaalame <ghenaalame66@gmail.com>
2025-04-23 19:01:20 +0300

    modified add order

--- src/app.ts
+++ src/app.ts
@@ -86,11 +86,14 @@ export class OrderManagment {
     getOrders() {
         return this.orders;
     }
-
     addOrder(item: string, price: number) {
-        const order: Order = { id: this.orders.length + 1, item, price };
-        this.validator.validate(order);
-        this.orders.push(order);
+        try {
+            const order: Order = { id: this.orders.length + 1, item, price };
+            this.validator.validate(order);
+            this.orders.push(order);
+        } catch (error: any) {
+            throw new Error("[OrderManagment] Error adding order: " + error.message);
+        }
     }
 
     getOrder(id: number) {
